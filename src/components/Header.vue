db68f73359693b1ba651888ea13dfc9cd70c328a
Irina Che <irina.che333@gmail.com>
2019-01-16 19:21:22 +0200

    Create Header and Menu components

---
+++ src/components/Header.vue
@@ -0,0 +1,73 @@
+<template>
+  <header>
+    <div id="logo"></div>
+    <div class="contacts">
+      <div class="icon"></div>
+      <div class="contacts_text">+38 067 47 85 459<br>+38 066 47 85 459</div>
+
+      <div class="icon"></div>
+      <div class="contacts_text">+38 067 47 85 459<br>+38 066 47 85 459</div>
+
+      <div class="icon"></div>
+      <div class="contacts_text">+38 067 47 85 459<br>+38 066 47 85 459</div>
+    </div>
+
+    <MainMenu />
+  </header>
+</template>
+
+<script>
+  import MainMenu from './MainMenu.vue'
+  export default {
+    components: {
+      MainMenu
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss"  scoped>
+  header{
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    background-color: white;
+  }
+
+  #logo{
+    background-image: url("../assets/images/logo.png");
+    background-repeat: no-repeat;
+    background-size: contain;
+    display: inline-block;
+    margin: 30px 80px;
+    height: 72px;
+    width: 154px;
+  }
+  .contacts{
+    position: absolute;
+    top:0;
+    right: 50px;
+    display: inline-block;
+    margin: 30px;
+
+
+  }
+  .icon{
+    display: inline-block;
+    width: 50px;
+    height: 50px;
+    border: 1px solid #acacac;
+    margin-right: 30px;
+    background-image: url("../assets/images/phone.png");
+    background-repeat: no-repeat;
+    background-size: 35px;
+    background-position: center;
+  }
+
+  .contacts_text{
+    display: inline-block;
+    color: black;
+
+  }
+</style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,90 @@
+<template>
+  <menu>
+    <a class="menuitem" href="#">
+      <div class="icon video_monitoring">  
+      </div>
+      <p v-html="services.video_monitoring"></p>
+    </a>
+    <a class="menuitem" href="#">
+      <div class="icon">  
+      </div>
+      <p v-html="services.access_control"></p>
+    </a>
+    <a class="menuitem" href="#">
+      <div class="icon">  
+      </div>
+      <p v-html="services.computer_networks"></p>
+    </a>
+    <a class="menuitem" href="#">
+      <div class="icon">  
+      </div>
+      <p v-html="services.intercom"></p>
+    </a>
+    <a class="menuitem" href="#">
+      <div class="icon">  
+      </div>
+      <p v-html="services.alarm"></p>
+    </a>    
+  </menu>
+</template>
+
+<script>
+  export default {
+     data() {
+      return{
+         services: {
+          video_monitoring: "Системы<br>видеонаблюдения" ,
+          access_control: "Системы<br>контроля доступа",
+          computer_networks:"Проектирование и монтаж<br>компьютерных сетей",
+          intercom:"Домофонные<br>системы",
+          alarm:"Пожарные и охранные<br>сигнализации"
+        }
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+  menu{
+    position: absolute;
+    top:80%;
+    width: 90%;
+    left: 0;
+    right: 0;
+    margin: auto;
+    padding: 0 5%;
+    display: flex;
+    flex-wrap: nowrap;
+  }
+  .menuitem {
+    padding: 10px 10px;
+    background-color: #2fc3b7;
+    width: 20%;
+    color: white;   
+     text-align: center; 
+  }
+  .menuitem:hover {
+    background-color: #5cd5cb;
+  }
+  .menuitem p{
+      display: inline-block;
+      text-align: center;
+      font-size: 10pt;
+      margin: 0;
+  }
+  .icon{
+    display: inline-block;
+    margin-right: 10px;
+    height: 34px;
+    width: 34px;
+    background-image: url("../assets/images/video_monitoring.png");
+    background-position: center;
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+  .video_monitoring{
+ //   background-image: url("../assets/images/video_monitoring.png");
+  }
+ 
+</style>
